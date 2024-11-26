ffb339d49dfe1d4b166817d0a65b7d252d7baca9
Vedant Bhasin <vedantbhasin@vedants-mbp-4.wifi.local.cmu.edu>
2024-11-26 15:14:42 -0500

    working

--- dlsys/apps/mlp.py
+++ dlsys/apps/mlp.py
@@ -3,15 +3,20 @@ from tvm import relax
 import numpy as np
 
 import sys
-sys.path.append('./python')
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'python')))
+
+
 import needle as ndl
 import needle.nn as nn
 from needle_tvm import to_tvm_tensor, to_tvm_fx
 
 class MLPModel(nn.Module):
     def __init__(self, n_layers=3, dim=512, activation=nn.ReLU, bias=False, device=None):
-        self.mlp = nn.Sequential(*[(nn.Linear(dim, dim, bias=bias), activation()) for _ in range(n_layers)])
-    
+            components = []
+            for _ in range(n_layers):
+                components.extend([nn.Linear(dim, dim, bias=bias, device=device), activation()])
+            self.mlp = nn.Sequential(*components)
     def forward(self, x):
         return self.mlp(x)
 
@@ -22,10 +27,11 @@ if __name__ == "__main__":
 
     # !important set placeholder = True
     x = np.array([[1, 2, 3, 4, 5]], dtype=np.float32)
-    input_tensor = Tensor(x, device=ndl.cpu(), requires_grad=False, placeholder=True)
+    input_tensor = ndl.Tensor(x, device=ndl.cpu(), requires_grad=False, placeholder=True)
     # print(f'input: {input_tensor.shape}')
 
-    mod = MLPModel(5, device=ndl.cpu())
+    mod = MLPModel(dim=5, device=ndl.cpu())
+    output_tensor = mod(input_tensor)
 
     # generate tvm IRModule using Tensor graph
     MLPModule = to_tvm_tensor(mod, input_tensor)
