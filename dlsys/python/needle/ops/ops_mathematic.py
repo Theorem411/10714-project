45f73b582097da5c6a0e903ae62d030b080c7595
and86802 <and86804@gmail.com>
2024-11-30 18:21:52 -0500

    add print

--- dlsys/python/needle/ops/ops_mathematic.py
+++ dlsys/python/needle/ops/ops_mathematic.py
@@ -34,7 +34,7 @@ class EWiseAdd(TensorOp):
         A = node_map[node.inputs[0]]
         B = node_map[node.inputs[1]]
         
-        print(A.numpy().shape, B.numpy().shape)
+        print(A, B)
         def te_ewise_add(A, B):
             return topi.add(A, B)
 
