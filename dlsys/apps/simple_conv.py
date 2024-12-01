19c251c1e0357ef022c1778a46bdc0f49a5befde
and86802 <and86804@gmail.com>
2024-11-30 20:21:50 -0500

    debug eval

--- dlsys/apps/simple_conv.py
+++ dlsys/apps/simple_conv.py
@@ -62,7 +62,7 @@ def evaluate_batch_conv(model, module, X: np.ndarray):
     with timer("tvm"):
         tvm_out = module["main"](input_tvm)
     tvm_time = time.perf_counter() - start_time
-    print(f"NDL output: {ndl_out}\tTVM output: {tvm_out}")
+    print(np.linalg.norm(tvm_out.asnumpy() - ndl_out.numpy()))
     assert np.allclose(tvm_out.asnumpy(), ndl_out.numpy(), atol=1e-4)
     return ndl_time, tvm_time
 
